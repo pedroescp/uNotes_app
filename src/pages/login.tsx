347d7da5ca260db66c18243c3b559bbcd948f4ef
Rafael Senac <vampirorp13@gmail.com>
2022-10-20 20:58:04 -0300

    login-page + theme

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,29 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 function Login() {
+	const formik = useFormik({
+		initialValues: {
+			usuario: '',
+			senha: '',
+			lembrar: false,
+		},
+		validationSchema: Yup.object({
+			usuario: Yup.string()
+				.min(5, 'O usuário deve ter no mínimo 5 caracteres.')
+				.max(50, 'O usuário deve ter no máximo 50 caracteres.')
+				.required('Usuário não informado.'),
+			senha: Yup.string()
+				.min(5, 'A senha deve ter no mínimo 5 caracteres.')
+				.max(50, 'A senha deve ter no máximo 50 caracteres.')
+				.required('Senha não informado.'),
+			lembrar: Yup.bool(),
+		}),
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
+
 	return (
 		<div className='flex justify-center items-center h-screen bg-slate-700'>
 			<div className='card w-96 bg-base-100 shadow-xl'>
@@ -7,49 +32,69 @@ function Login() {
 						<label className='swap swap-flip text-xl '>
 							<input type='checkbox' defaultChecked />
 							<h2
-								className='swap-on tooltip tooltip-open tooltip-primary tooltip-top mt-3'
+								className='swap-on tooltip tooltip-open tooltip-secondary tooltip-top mt-3'
 								data-tip='Novo? Clique aqui!'
 							>
 								Entrar
 							</h2>
-							<h2 className='swap-off'>Registro</h2>
+							<h2 className='swap-off'>Registrar</h2>
 						</label>
 					</div>
 					<div className='divider m-0'></div>
-					<form className='form-control w-full max-w-xl'>
-						<div className=''>
+					<form
+						onSubmit={formik.handleSubmit}
+						className='form-control w-full max-w-xl'
+					>
+						<label htmlFor='usuario' className='label'>
+							<span className='label-text'>Usuário / E-mail</span>
+						</label>
+						<input
+							id='usuario'
+							name='usuario'
+							type='text'
+							className='input input-bordered w-full max-w-xl'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.usuario}
+						/>
+						{formik.touched.usuario && formik.errors.usuario ? (
 							<label className='label'>
-								<span className='label-text'>
-									Usuário / E-mail
-								</span>
-							</label>
-							<input
-								type='text'
-								className='input input-bordered w-full max-w-xl'
-							/>
-							<label className='label hidden'>
 								<span className='label-text-alt text-error'>
-									Mensagem de erro padrão
+									{formik.errors.usuario}
 								</span>
 							</label>
-						</div>
-						<div className=''>
+						) : null}
+
+						<label htmlFor='senha' className='label'>
+							<span className='label-text'>Senha</span>
+						</label>
+						<input
+							id='senha'
+							name='senha'
+							type='password'
+							className='input input-bordered w-full max-w-xl'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.senha}
+						/>
+						{formik.touched.senha && formik.errors.senha ? (
 							<label className='label'>
-								<span className='label-text'>Senha</span>
-							</label>
-							<input
-								type='text'
-								className='input input-bordered w-full max-w-xl'
-							/>
-							<label className='label hidden'>
 								<span className='label-text-alt text-error'>
-									Mensagem de erro padrão
+									{formik.errors.senha}
 								</span>
 							</label>
-						</div>
+						) : null}
+
 						<div className='flex flex-col items-start mb-5 gap-2'>
 							<label className='label cursor-pointer'>
-								<input type='checkbox' className='checkbox' />
+								<input
+									id='lembrar'
+									name='lembrar'
+									type='checkbox'
+									className='checkbox'
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+								/>
 								<span className='label-text pl-4'>
 									Lembrar-me
 								</span>
@@ -58,7 +103,10 @@ function Login() {
 						</div>
 
 						<div className='flex flex-col items-center'>
-							<button className='btn btn-primary btn-wide'>
+							<button
+								type='submit'
+								className='btn btn-primary btn-wide'
+							>
 								Entrar
 							</button>
 						</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,36 @@ module.exports = {
 	plugins: [require('daisyui')],
 	daisyui: {
 		styled: true,
-		themes: true,
+		themes: [
+			{
+				mytheme: {
+					primary: '#F28C18',
+					secondary: '#00a0c4',
+					accent: '#D926A9',
+					neutral: '#191D24',
+					'base-100': '#2A303C',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272',
+				},
+			},
+		],
+		// [
+		// 	{
+		// 		mytheme: {
+		// 			primary: '#F28C18',
+		// 			secondary: '#00a0c4',
+		// 			accent: '#e0e0e0',
+		// 			neutral: '#1B1D1D',
+		// 			'base-100': '#212121',
+		// 			info: '#2463EB',
+		// 			success: '#16A249',
+		// 			warning: '#FBBD23',
+		// 			error: '#DC2828',
+		// 		},
+		// 	},
+		// ]
 		base: true,
 		utils: true,
 		logs: true,
