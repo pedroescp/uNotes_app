53365e4518d962a97b24437e6053ff7f02c711f1
pedroescp <pedrohhss14@gmail.com>
2022-11-09 20:57:01 -0300

    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { HomePage } from "../components/homepageComponent";
 import { ProfileNavbar } from "../components/profileNavbarComponent";
-import Search from "../components/search";
+
 
 function Home() {
   return (
