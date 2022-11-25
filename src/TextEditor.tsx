d752fd86168e620b52cde8ab20d91ddf626cc7ab
Rafael Senac <vampirorp13@gmail.com>
2022-11-25 19:59:02 -0300

    Bookmark

--- src/TextEditor.tsx
+++ src/TextEditor.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import 'quill/dist/quill.snow.css';
 import Quill from 'quill';
 import { io } from 'socket.io-client';
+import { Bookmark } from './images/icons/icons';
 
 //header of the lib
 const TOOLBAR_OPTIONS = [
@@ -85,38 +86,12 @@ function TextEditor({ note }: Parameters) {
           type='text'
           placeholder='Titulo'
           maxLength={30}
-          value={note?.title}
+          defaultValue={note?.title}
           className='input input-ghost w-full bg-transparent focus:outline-none'
         />
-        <label className='swap'>
-          <input type='checkbox' />
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='swap-on w-8 h-6 mr-5 '
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z'
-            />
-          </svg>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='currentColor'
-            className='swap-off w-8 h-6 mr-5'
-          >
-            <path
-              fill-rule='evenodd'
-              d='M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z'
-              clip-rule='evenodd'
-            />
-          </svg>
-        </label>
+        <div className='mr-5'>
+          <Bookmark />
+        </div>
       </div>
       <div className='container' id='container' ref={wrapperRef}></div>
     </>
