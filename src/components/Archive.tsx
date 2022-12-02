0414f89a113d7516a2035e6f306e114f38a31d70
pedroescp <pedrohhss14@gmail.com>
2022-12-01 21:16:06 -0300

    feat: change the archive to archivecharge

---
+++ src/components/Archive.tsx
@@ -0,0 +1,97 @@
+import { useState, useRef, useEffect } from 'react';
+import { Bookmark } from '../images/icons/icons';
+import { NotesModal } from './notesModal';
+import api from '../utils/api';
+
+interface Parameters {
+  type: string;
+}
+
+export function ArchiveCharges({ type }: Parameters) {
+  const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
+  const [notes, setArchive] = useState([]);
+  const [note, setOnlyNote] = useState([]);
+
+  useEffect(() => {
+    async function getArchive() {
+      //make a filter
+      const response = await api.archivehGet();
+      setArchive(response.data);
+    }
+    getArchive();
+  }, []);
+
+  const openButtonRef = (note: any) => {
+    setOnlyNote(note);
+    setOpen(!open);
+  };
+
+  const handleOnArchiveModalClose = ({ title, text }: any) => {
+    async function notesUpdate() {
+      //make a filter
+      await api.notesUpdate({
+        id: note['id'],
+        titulo: title,
+        texto: text,
+        criadorId: 'f0f1b37b-23ec-47ad-b777-307ace3e45ae',
+        usuarioAtualizacaoId: 'f0f1b37b-23ec-47ad-b777-307ace3e45ae',
+        documentoId: null,
+      });
+    } 
+    async function getArchive() {
+      //make a filter
+      const response = await api.notesGet();
+      setArchive(response.data);
+    }
+    notesUpdate()
+    getArchive();
+  };
+
+  return (
+    <div className='mx-auto max-w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center mt-20 '>
+        {notes.map((note: any) => (
+          <div
+            onClick={() => openButtonRef(note)}
+            key={note.id}
+            id={note.id}
+            
+            
+            className='w-full h-fit'
+          >
+            <div className='card bg-secondary text-primary-content cursor-pointer h-full'>
+              <div className='card-body p-4'>
+                <div className='flex'>
+                  <h2 className='card-title pl-2 justify-between w-[90%] break-all'>
+                    {note.titulo}
+                  </h2>
+                  <span className='px-2'>
+                    <Bookmark key={note.bookmark} onClick={console.log(note)} />
+                  </span>
+                </div>
+
+                <p>
+                  <div
+                    className='ql-editor p-0 card_preview-text h-full'
+                    data-gramm='false'
+                    data-placeholder='Escrever uma nota'
+                  >
+                    {note.texto}
+                  </div>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <NotesModal
+        open={open}
+        cancelButtonRef={cancelButtonRef}
+        setOpen={setOpen}
+        note={note}
+        onClose={handleOnArchiveModalClose}
+      />
+    </div>
+  );
+}
