4235f3a9a47d0b56ed729ed6079e6af530454628
Carlos <Carlos.pauli@outlook.com>
2022-12-08 21:05:10 -0300

    Adicionado os dados do usuario vindo da API

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,29 +1,48 @@
+import { useEffect, useState } from 'react';
 import NavBar from '../components/navBar';
+import { UserIcon } from '../images/icons/icons';
+import api from '../utils/api';
 
 const Profile = () => {
-  return (
-    <>
-      <NavBar>
-        <div className='container justify-center items-center'>
-          <div className='card w-96 bg-base-100 shadow-xl'>
-            <figure className='px-10 pt-10'>
-              <div className='avatar online'>
-                <div className='w-24 rounded-full'>
-                  <img src='https://placeimg.com/192/192/people' />
+    const [usuarios, setUsuario] = useState([]);
+
+    useEffect(() => {
+        async function getUsuario() {
+            //make a filter
+            const response = await api.usuarioGet()
+            setUsuario(response.data)
+        }
+        getUsuario();
+
+
+    }, []);
+
+
+    return (
+        <>
+            <NavBar>
+                <div className='container justify-center items-center'>
+                    <div className='card w-96 bg-base-100 shadow-xl'>
+                        <figure className='px-10 pt-10'>
+                            <div className='avatar online'>
+                                <div className='w-24 rounded-full'>
+                                    <img src='https://placeimg.com/192/192/people' />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className='card-body items-center text-center'>
+                            <h2 className='card-title'>
+                                Hello {(usuarios['nome'])}
+                            </h2>
+                            <p>Nickname: {(usuarios['login'])}</p>
+                            <p>E-mail: {(usuarios['email'])}</p>
+                            <p> Telefone: {(usuarios['telefone'])}</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </figure>
-            <div className='card-body items-center text-center'>
-              <h2 className='card-title'>
-                Hello {String(JSON.parse(localStorage.getItem('user')).user)}
-              </h2>
-              <p>Email: {String(JSON.parse(localStorage.getItem('user')).email)}</p>
-            </div>
-          </div>
-        </div>
-      </NavBar>
-    </>
-  );
+            </NavBar>
+        </>
+    );
 };
 
 export default Profile;
