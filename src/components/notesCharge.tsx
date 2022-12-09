4368efbabea8848e78ff46737e233f2a5e40228b
pedroescp <pedrohhss14@gmail.com>
2022-12-08 21:04:50 -0300

    Feat: add a loding when the requisition dont finish

--- src/components/notesCharge.tsx
+++ src/components/notesCharge.tsx
@@ -3,6 +3,7 @@ import { Bookmark } from '../images/icons/icons';
 import { NotesModal } from './notesModal';
 import NotesFAB from '../components/noteCreate';
 import api from '../utils/api';
+import { SkeletonNotes } from './skeletonNotes';
 
 interface Parameters {
   type: string;
@@ -13,12 +14,14 @@ export function NotesCharges({ type }: Parameters) {
   const cancelButtonRef = useRef(null);
   const [notes, setNotes] = useState([]);
   const [note, setOnlyNote] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getNotes() {
       //make a filter
       const response = await api.notesGet();
       setNotes(response.data);
+        setLoading(false);
     }
     getNotes();
   }, []);
@@ -49,39 +52,46 @@ export function NotesCharges({ type }: Parameters) {
 
   return (
     <>
-      <div className='mx-auto max-w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center mt-20 '>
-          {notes.map((note: any) => (
-            <div
-              onClick={() => openButtonRef(note)}
-              key={note.id}
-              id={note.id}
-              className='w-full h-fit'
-            >
-              <div className='card bg-secondary text-primary-content cursor-pointer h-full'>
-                <div className='card-body p-4'>
-                  <div className='flex'>
-                    <h2 className='card-title pl-2 justify-between w-[90%] break-all'>
-                      {note.titulo}
-                    </h2>
-                    <span className='px-2'>
-                      <Bookmark />
-                    </span>
-                  </div>
-
-                  <p>
-                    <div
-                      className='ql-editor p-0 card_preview-text h-full'
-                      data-gramm='false'
-                      data-placeholder='Escrever uma nota'
-                    >
-                      {note.texto}
+      <div
+        data-aos='zoom-out'
+        data-aos-anchor-placement='top-center'
+        className='mx-auto max-w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl'
+      >
+        <div className='columns-2 md:columns-3 lg:columns-4 mt-20'>
+          
+          {loading && <SkeletonNotes />}
+          {!loading &&
+            notes.map((note: any) => (
+              <div
+                onClick={() => openButtonRef(note)}
+                key={note.id}
+                id={note.id}
+                className='w-full h-fit'
+              >
+                <div className='card bg-secondary text-primary-content cursor-pointer relative mb-4'>
+                  <div className='card-body p-4'>
+                    <div className='flex'>
+                      <h2 className='card-title pl-2 justify-between w-[90%] break-all'>
+                        {note.titulo}
+                      </h2>
+                      <span className='px-2'>
+                        <Bookmark />
+                      </span>
                     </div>
-                  </p>
+
+                    <p>
+                      <div
+                        className='ql-editor p-0 card_preview-text h-full'
+                        data-gramm='false'
+                        data-placeholder='Escrever uma nota'
+                      >
+                        {note.texto}
+                      </div>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
         <NotesModal
           open={open}
--- src/components/trashCharge.tsx
+++ src/components/trashCharge.tsx
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect } from 'react';
 import { Bookmark } from '../images/icons/icons';
 import { NotesModal } from './notesModal';
 import api from '../utils/api';
+import { SkeletonNotes } from './skeletonNotes';
 
 interface Parameters {
   type: string;
@@ -12,12 +13,14 @@ export function TrashCharges({ type }: Parameters) {
   const cancelButtonRef = useRef(null);
   const [notes, setTrash] = useState([]);
   const [note, setOnlyNote] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getTrash() {
       //make a filter
       const response = await api.trashGet();
       setTrash(response.data);
+      setLoading(false)
     }
     getTrash();
   }, []);
@@ -28,16 +31,21 @@ export function TrashCharges({ type }: Parameters) {
   };
 
   return (
-    <div className='mx-auto max-w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center mt-20 '>
-        {notes.map((note: any) => (
+    <div
+      data-aos='zoom-out'
+      data-aos-anchor-placement='top-center'
+      className='mx-auto max-w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl'
+    >
+      <div className='columns-2 md:columns-3 lg:columns-4 mt-20'>
+      {loading && <SkeletonNotes />}
+          {!loading &&notes.map((note: any) => (
           <div
             onClick={() => openButtonRef(note)}
             key={note.id}
             id={note.id}
             className='w-full h-fit'
           >
-            <div className='card bg-secondary text-primary-content cursor-pointer h-full'>
+            <div className='card bg-secondary text-primary-content cursor-pointer relative mb-4'>
               <div className='card-body p-4'>
                 <div className='flex'>
                   <h2 className='card-title pl-2 justify-between w-[90%] break-all'>
