e9302629722eb01d78d0fc00da04bd0086a942c0
pedro <pedrohhss14@gmail.com>
2023-03-05 21:33:55 -0300

    Feat: create a componete empty

---
+++ src/components/Empty.tsx
@@ -0,0 +1,15 @@
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+export function Empty() {
+  return (
+    <>
+      <Player
+        autoplay
+        loop
+        src='src\images\lottiefiles\empty.json'
+        style={{ height: '300px', width: '300px' }}
+      ></Player>
+      <h1>Parece que por aqui esta vazio</h1>
+    </>
+  );
+}
