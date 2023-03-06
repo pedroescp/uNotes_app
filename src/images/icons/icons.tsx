6bcb5f4cdfed7c76eee9316d10e96fcd27d2a5a8
pedro <pedrohhss14@gmail.com>
2023-03-05 21:35:16 -0300

    Feat: change a icon and add a new icon

--- src/images/icons/icons.tsx
+++ src/images/icons/icons.tsx
@@ -4,14 +4,14 @@ export function HamburgerIcon() {
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
       viewBox='0 0 24 24'
-      strokeWidth={1.5}
+      stroke-width='1.5'
       stroke='currentColor'
-      className='w-6 h-6'
+      class='w-6 h-6'
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
       />
     </svg>
   );
@@ -299,6 +299,25 @@ export function NoteIcon() {
         strokeLinecap='round'
         strokeLinejoin='round'
         d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
+      />
+    </svg>
+  );
+}
+
+export function DocumentIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke-width='1.5'
+      stroke='currentColor'
+      class='w-6 h-6'
+    >
+      <path
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        d='M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z'
       />
     </svg>
   );
