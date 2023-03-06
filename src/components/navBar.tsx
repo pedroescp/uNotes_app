97d3b17ffd117017980f78e3cb7893bc3ea62115
pedro <pedrohhss14@gmail.com>
2023-03-05 21:35:43 -0300

    Feat: change the navbar to another fixed and diferent

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,6 +1,7 @@
 import {
   ArchiveIcon,
   ArrowBack,
+  DocumentIcon,
   ExitMarkIcon,
   Group,
   HamburgerIcon,
@@ -33,38 +34,29 @@ const NavBar = ({ children }: Props) => {
   function ProfileComponent() {
     return (
       <>
-        <div className={classes()} tabIndex={0}>
-          <div className='flex gap-2 cursor-pointer'>
-            <label className='btn btn-ghost btn-circle avatar online'>
-              <div className='w-10 rounded-full'>
-                <img src='https://placeimg.com/80/80/people' />
-              </div>
-            </label>
-            <span className='text-start line-clamp-1 w-32'>
-              <span className='text-lg'>
-                {String(JSON.parse(localStorage.getItem('user')).user)}
-              </span>
-              <p className='text-xs capitalize'>Desenvolvedor</p>
-            </span>
+        <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+          <div className='w-10 rounded-full'>
+            {/* <img src='src\images\icons\' /> */}
           </div>
-          <ul
-            tabIndex={0}
-            className='menu dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52 lg:mt-52'
-          >
-            <li>
-              <a onClick={() => navigate('/profile')}>
-                <UserIcon />
-                Perfil
-              </a>
-            </li>
-            <li>
-              <label htmlFor='exit-site'>
-                <LogoutIcon />
-                Sair
-              </label>
-            </li>
-          </ul>
-        </div>
+        </label>
+        <ul
+          tabIndex={0}
+          className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
+        >
+          <li>
+            <a onClick={() => navigate('/profile')}>
+              <UserIcon />
+              Perfil
+              <span className='badge'>New</span>
+            </a>
+          </li>
+          <li>
+            <label htmlFor='exit-site'>
+              <LogoutIcon />
+              Sair
+            </label>
+          </li>
+        </ul>
 
         <ExitSiteMesssage htmlFor='exit-site' />
       </>
@@ -73,22 +65,18 @@ const NavBar = ({ children }: Props) => {
 
   function SearchComponent() {
     return (
-      <div className='bg-base-200 w-full mx-auto sm:w-96 md:w-3/4 lg:w-1/2 p-1 rounded-full'>
-        <div className='flex gap-2'>
-          <label
-            htmlFor='menu-lateral'
-            className='btn rounded-full p-0 w-full max-w-[3rem] h-auto max-h-12 min-h-0 drawer-button'
-          >
-            <HamburgerIcon />
-          </label>
-          <input
-            type='text'
-            placeholder='Toque para filtrar notas'
-            className='input input-sm px-2 w-full rounded-full bg-base-200 transition-all focus:input-md focus:bg-base-100 focus:outline-none active:outline-none active:outline-0'
-          />
-          <button className='btn rounded-full p-0 w-full max-w-[3rem] h-auto max-h-12 min-h-0 drawer-button hidden sm:flex'>
-            <SearchIcon />
-          </button>
+      <div className='menu menu-horizontal px-1'>
+        <div className='bg-base-200 w-full mx-auto sm:w-1/2 md:w-3/4 lg:w-96 p-1 rounded-full'>
+          <div className='flex gap-2'>
+            <input
+              type='text'
+              placeholder='Toque para filtrar notas'
+              className='input input-sm px-2 w-full rounded-full bg-base-200 transition-all focus:input-md focus:bg-base-100 focus:outline-none active:outline-none active:outline-0'
+            />
+            <button className='btn rounded-full p-0 w-full max-w-[3rem] h-auto max-h-12 min-h-0 drawer-button hidden sm:flex'>
+              <SearchIcon />
+            </button>
+          </div>
         </div>
       </div>
     );
@@ -119,6 +107,12 @@ const NavBar = ({ children }: Props) => {
             </a>
           </li>
           <li>
+            <a onClick={() => navigate('/document')}>
+              <DocumentIcon />
+              Documentos
+            </a>
+          </li>
+          <li>
             <a onClick={() => navigate('/archive')}>
               <ArchiveIcon />
               Arquivo
@@ -132,7 +126,7 @@ const NavBar = ({ children }: Props) => {
           </li>
           <li>
             <a onClick={() => navigate('/group')}>
-              <Group/>
+              <Group />
               Grupo
             </a>
           </li>
@@ -180,14 +174,30 @@ const NavBar = ({ children }: Props) => {
 
   return (
     <>
-      {ProfileComponent()}
       <div className='drawer'>
         <input id='menu-lateral' type='checkbox' className='drawer-toggle' />
 
-        <div className='drawer-content flex flex-col p-4 xl:items-center'>
-          {SearchComponent()}
-          {/* Conteúdo da página */ children}
+        <div className='drawer-content flex flex-col xl:items-center'>
+          <div className='navbar bg-base-100'>
+            <div className='navbar-start'>
+              <label
+                htmlFor='menu-lateral'
+                className='btn btn-square btn-ghost'
+              >
+                <HamburgerIcon />
+              </label>
+              <a className='btn btn-ghost normal-case text-xl' onClick={() => navigate('/home')}>
+                Notes Hub
+              </a>
+            </div>
+            <div className='navbar-center hidden lg:flex'>{SearchComponent()}</div>
+            <div className='navbar-end'>
+              <div className='dropdown dropdown-end'>{ProfileComponent()}</div>
+            </div>
+          </div>
+           {/* Conteúdo da página */ children}
         </div>
+
         <div className='drawer-side'>{DrawerComponent()}</div>
       </div>
     </>
