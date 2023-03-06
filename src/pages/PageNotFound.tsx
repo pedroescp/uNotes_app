8635c2600da5de3c5c3f0977c197fdb9867f2431
pedro <pedrohhss14@gmail.com>
2023-03-06 18:56:01 -0300

    Feat: add a message 404 not found and redirect user to home

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,10 +1,25 @@
+import { Player } from '@lottiefiles/react-lottie-player';
+import { useNavigate } from 'react-router-dom';
+
 function PageNotFound() {
-    return (
-      <div className="App">
-        <h1 className="text-3xl font-bold underline">404</h1>
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex justify-center h-[calc(100vh_-_80px)] lg:h-screen flex-direction-column items-center'>
+      <div className='container px-4'>
+        <div className='flex flex-col justify-center items-center h-full  py-4 gap-8  '>
+          <Player
+            autoplay
+            loop
+            src='src\images\lottiefiles\404.json'
+            style={{ height: '300px', width: '300px' }}
+          ></Player>
+          <h1>Pagina nao encontratada</h1>
+          <button className='btn btn-primary btn-wide' onClick={() => navigate('/home')}>Voltar para o inicio</button>
+        </div>
       </div>
-    );
-  }
-  
-  export default PageNotFound;
-  
+    </div>
+  );
+}
+
+export default PageNotFound;
